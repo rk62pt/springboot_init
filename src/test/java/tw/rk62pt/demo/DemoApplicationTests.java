e6a3fca7cd5aad767b519bce7101c23d44931a7b
rk62pt <rk62pt@gmail.com>
2020-10-31 15:59:48 +0800

    init

---
+++ src/test/java/tw/rk62pt/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.rk62pt.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
